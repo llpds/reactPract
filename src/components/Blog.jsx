7529004f86139638c9447073ec8906ba1c363d87
llpds <95649605+llpds@users.noreply.github.com>
2023-12-11 22:00:34 +0200

    form_object

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Blog = ({listNumber, blog, like, deleteBlog}) => {
   return (
     <li key={blog.id}>
-      {listNumber+1}. title: {blog.title} body: {blog.body} likes: {blog.likes} 
+      {listNumber}. title: {blog.title} body: {blog.body} likes: {blog.likes} 
       <button onClick={() => like(blog)}>like</button>
       <button onClick={() => deleteBlog(blog)}>delete</button>
     </li>
